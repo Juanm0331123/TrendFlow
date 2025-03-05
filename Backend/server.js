3269f56ae802dccedee35d603ef19c22b05290cb
Juan Miguel León Gómez <85261235+Juanm0331123@users.noreply.github.com>
2025-03-04 22:39:38 -0500

    Conección a la base de datos de mongo, configuración e instalación de dependencias en backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./database/connection.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 3000;
+dotenv.config();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT} ✈️`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+  }
+};
+
+startServer();
